a648d0d51a666eeaf4f832f94a58ccb0e38455e1
medzz1osm <112688860+medzz1osm@users.noreply.github.com>
2023-10-12 23:02:55 +0200

    Add files via upload
    la til oppgave 8 
    og oppgave 9 halv ferdig

---
+++ oppgave8.py
@@ -0,0 +1,10 @@
+min_liste = [3, 132, 14]
+
+print("Første tallet i min liste:", min_liste[0]) 
+ling = []
+
+ling.append("hei")
+ling.append("verden")
+
+# Skriv ut hele lista ling
+print("Hele lista ling er:", ling)
